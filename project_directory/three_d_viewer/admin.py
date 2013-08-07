88fe7b7e797cf029ad50cede0bf22857d89b8729
Shane Frischkorn <shanef@gmail.com>
2013-08-07 22:47:31 +1000

    Added description and question and answer models

--- project_directory/three_d_viewer/admin.py
+++ project_directory/three_d_viewer/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from three_d_viewer.models import Category, Sample
+from three_d_viewer.models import Category, Sample, Question, Answer
 
 admin.site.register(Category)
 admin.site.register(Sample)
+admin.site.register(Question)
+admin.site.register(Answer)
--- project_directory/three_d_viewer/models.py
+++ project_directory/three_d_viewer/models.py
@@ -20,5 +20,25 @@ class Category(CommonInfo):
 
 class Sample(CommonInfo):
     model_filename = models.CharField(max_length=1000)
+    description = models.CharField(max_length=2000, default='', blank=True, null=True)
     parent = models.ForeignKey(Category, blank=True, null=True,
                                on_delete=models.SET_NULL)
+
+
+class Question(models.Model):
+    text = models.CharField(max_length=2000)
+    sample = models.ForeignKey(Sample, related_name='questions')
+
+    def correct_answers(self):
+        return self.answers.filter(correct = True)
+    
+    def __unicode__(self):
+        return self.text
+    
+class Answer(models.Model):
+    text = models.CharField(max_length=2000)
+    correct = models.BooleanField(default=False)
+    question = models.ForeignKey(Question, related_name='answers')
+    
+    def __unicode__(self):
+        return self.text
