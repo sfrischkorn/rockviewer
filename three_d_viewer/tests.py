e4b18d98c2e92fb37273defba5e840cbb65513c8
Shane Frischkorn <shanef@gmail.com>
2013-08-24 23:53:30 +1000

    Added some tests for models.py

--- three_d_viewer/tests.py
+++ three_d_viewer/tests.py
@@ -6,11 +6,40 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from three_d_viewer.models import Category, Question, Answer
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class ModelsTest(TestCase):
+    #def setUp(self):
+      #CommonInfo.objects.create(name="test", active=True)
+      #  Animal.objects.create(name="cat", sound="meow")
+        
+    def test_common_unicode(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests the unicode method for categories
         """
-        self.assertEqual(1 + 1, 2)
+        testcat = Category(name='test', active=True)
+        self.assertEqual("test", testcat.__unicode__())
+    
+    def test_question_unicode(self):
+        """
+        Tests the unicode method for questions
+        """
+        
+        testquestion = Question(text="Test")
+        self.assertEqual("Test", testquestion.__unicode__())
+        
+    def test_answer_unicode(self):
+        """
+        Tests the unicode method for answers
+        """
+        
+        testanswer = Answer(text="Test")
+        self.assertEqual("Test", testanswer.__unicode__())
+    
+    def test_no_correct_answers(self):
+        testquestion = Question(text="Test")
+        
+        Answer(text="Test", correct=False, question=testquestion)
+        
+        self.assertEqual(len(testquestion.correct_answers()), 0)
